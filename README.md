db94ff1a0e50deee66681a159969afbf81520cb2
JiapengLi <mail@jiapeng.me>
2021-01-20 17:26:16 +0800

    Update README and fix minor bugs

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# lorcalc
-LoRa Calculator - command line tool to calculate lora packet
+# LoRa Calculator 
+**A simple command line tool for lora packet calculation.**
+
+So, finally I am tired of the Semtech GUI tool and the customized Excel shits, let's move to the elegant cmdline tool together . 
+
+## Feature
+
+All below parameters are configurable:
+
+- SF5 - SF12 spread factor
+- Long Interleaving
+- Low Data Rate Optimize
+- Header
+- CRC
+- Prebuilt binary for Windows platform
+
+## Compile
+
+For windows platform, please install mingw first. A prebuilt binary is included in the repo. 
+
+```bash
+$ gcc lrc.c -o lrc
+```
+
+## Usage
+
+```
+./lrc -h
+Usage: lrc [OPTIONS]
+ -h, --help                                      Help
+ -v, --version                                   Version 0.0.1
+ -b, --bw,   --bandwidth                 <uint>  Bandwidth in hz, eg: 125000, 250000, 203125
+ -s, --sf,   --spread-factor             <uint>  Spread factor (5 - 12)
+ -o, --ldro, --low-datarate-optimize     <bool>  Low datarate optimize (on / off)
+ -f, --fs,   --fine-synch                <bool>  Fine synch for high speed lora SF5 and SF6,
+ -i, --li,   --long-interleaving         <bool>  Available in 2.4GHz lora (sx128x)
+ -c, --cr,   --coderate                  <uint>  coding rate CR4/5 CR4/6... (1 - 4)
+ -r, --pr,   --preamble                  <uint>  Raw preamble lenght
+ -d, --hdr,  --header                    <bool>  Header option (on / off)
+ -k, --crc,                              <bool>  CRC option (on / off)
+ -l, --pl,   --payload                   <uint>  payload length
+```
+
+## Example
+
+### Default Paramters
+
+```
+./lrc
+
+sf 7, bw 125000, cr 1, ldro off, fs off, li off, pr 8, hdr on, crc on, pl 12
+toa: 41216us, 41.216ms, 0.041s
+```
+
+![image-20210120163914135](https://img.juzuq.com/20210120-163918-123.png)
+
+## SF12 with Low Data Rate Optimize On
+
+```
+./lrc --sf=12
+
+toa: 1155072us, 1155.072ms, 1.155s
+sf 12, bw 125000, cr 1, ldro on, fs off, li off, pr 8, hdr on, crc on, pl 12
+```
+
+![image-20210120164441045](https://img.juzuq.com/20210120-164443-628.png)
+
+### SF12 with Low Data Rate Optimize Off
+
+```
+./lrc --sf=12 --ldro=off
+
+toa: 991232us, 991.232ms, 0.991s
+sf 12, bw 125000, cr 1, ldro off, fs off, li off, pr 8, hdr on, crc on, pl 12
+```
+
+![image-20210120164513969](https://img.juzuq.com/20210120-164612-732.png)
+
+## Acknowledgement 
+
+- Original calculator API https://github.com/Lora-net/gateway_2g4_hal/blob/master/libloragw/src/loragw_hal.c#L492
+- Some code are copied from https://github.com/JiapengLi/lorawan-parser/blob/master/util/parser/main.c
--- lrc.c
+++ lrc.c
@@ -101,12 +101,16 @@ double lrc(opt_t *opt)
     fine_synch = (datarate <= 6);
     if (opt->fs != BOOL_AUTO) {
         fine_synch = opt->fs;
+    } else {
+        opt->fs = fine_synch;
     }
 
     /* option #2: long interleaving */
     long_interleaving = (coderate > 4);
     if (opt->li != BOOL_AUTO) {
         long_interleaving = opt->li;
+    } else {
+        opt->li = long_interleaving;
     }
     if (long_interleaving) {
         int cr_is_7 = (coderate == 7) ? 1 : 0;
@@ -119,6 +123,8 @@ double lrc(opt_t *opt)
     low_datarate_optimize = (datarate >= 11);
     if (opt->ldro != BOOL_AUTO) {
         low_datarate_optimize = opt->ldro;
+    } else {
+        opt->ldro = low_datarate_optimize;
     }
     tx_bits_symbol = datarate - 2 * (low_datarate_optimize ? 1 : 0);
 
@@ -161,175 +167,6 @@ double lrc(opt_t *opt)
 
     return LocalTimeOnAir;
 }
-
-void usage(void)
-{
-    print("help info, TBD");
-}
-
-enum {
-    ERR_PARA            = -1,
-
-};
-
-bool get_u32(const char * const s, uint32_t *v)
-{
-    char *ptr;
-    uint32_t tmp;
-
-    tmp = strtoul(optarg, &ptr, 10);
-    if( (*ptr != '\0') || (tmp < 0) ){
-        return false;
-    }
-
-    *v = tmp;
-
-    return true;
-}
-
-bool get_bool(const char * const s, bool *bl)
-{
-    char *ptr;
-    uint32_t tmp;
-
-    if ((0 == strcmp(s, "true")) || (0 == strcmp(s, "on")) ) {
-        *bl = true;
-    } else if ((0 == strcmp(s, "false")) || (0 == strcmp(s, "off")) ) {
-        *bl = false;
-    } else if (get_u32(s, &tmp)) {
-        *bl = (bool)tmp;
-    } else {
-        return false;
-    }
-    return true;
-}
-
-const char * const str_auto_bool(auto_bool_t abl)
-{
-    switch (abl) {
-    case BOOL_AUTO:
-        return "auto";
-    case BOOL_TRUE:
-        return "on";
-    case BOOL_FALSE:
-        return "off";
-    }
-    return "unknown";
-}
-
-const char * const str_bool(bool bl)
-{
-    if (bl) {
-        return "on";
-    } else {
-        return "off";
-    }
-}
-
-int main(int argc, char **argv)
-{
-    opt_t app_opt;
-    opt_t *opt;
-    int ret, index, hlen, i;
-    char *ptr;
-    uint32_t tmp;
-
-    opt = &app_opt;
-    opt->sf = 7;
-    opt->bw = 125000;
-    opt->cr = 1;
-    opt->ldro = BOOL_AUTO;
-    opt->fs = BOOL_AUTO;
-    opt->li = BOOL_AUTO;
-    opt->pr = 8;
-    opt->hdr = true;
-    opt->crc = true;
-    opt->pl = 12;
-
-    while (1) {
-        ret = getopt_long(argc, argv, ":hvs:b:c:o:f:i:r:d:k:l:", app_long_options, &index);
-        if (ret == -1) {
-            break;
-        }
-        switch (ret) {
-        case 'h':
-            usage();
-            return 0;
-        case 'v':
-
-            return 0;
-        case 's':
-            if (!get_u32(optarg, &opt->sf)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'b':
-            if (!get_u32(optarg, &opt->bw)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'c':
-            if (!get_u32(optarg, &opt->cr)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'o':
-            if (!get_u32(optarg, &opt->ldro)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'f':
-            if (!get_u32(optarg, &opt->fs)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'i':
-            if (!get_u32(optarg, &opt->li)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'r':
-            if (!get_u32(optarg, &opt->pr)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'd':
-            if (!get_bool(optarg, &opt->hdr)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'k':
-            if (!get_bool(optarg, &opt->crc)) {
-                return ERR_PARA;
-            }
-            break;
-        case 'l':
-            if (!get_u32(optarg, &opt->pl)) {
-                return ERR_PARA;
-            }
-            break;
-        }
-    }
-
-    print("sf %u, bw %u, cr %u, ldro %s, fs %s, li %s, pr %u, hdr %s, crc %s, pl %u",
-        opt->sf,
-        opt->bw,
-        opt->cr,
-        str_auto_bool(opt->ldro),
-        str_auto_bool(opt->fs),
-        str_auto_bool(opt->li),
-        opt->pr,
-        str_bool(opt->hdr),
-        str_bool(opt->crc),
-        opt->pl
-        );
-    printf("toa: %.3lf\n", lrc(opt));
-
-    return 0;
-}
-
-
-
 
 int print(char *fmt, ...)
 {
@@ -428,4 +265,193 @@ int print(char *fmt, ...)
 
 
     return i;
+}
+
+bool get_u32(const char * const s, uint32_t *v)
+{
+    char *ptr;
+    uint32_t tmp;
+
+    tmp = strtoul(optarg, &ptr, 10);
+    if( (*ptr != '\0') || (tmp < 0) ){
+        return false;
+    }
+
+    *v = tmp;
+
+    return true;
+}
+
+bool get_bool(const char * const s, bool *bl)
+{
+    char *ptr;
+    uint32_t tmp;
+
+    if ((0 == strcmp(s, "true")) || (0 == strcmp(s, "on")) ) {
+        *bl = true;
+    } else if ((0 == strcmp(s, "false")) || (0 == strcmp(s, "off")) ) {
+        *bl = false;
+    } else if (get_u32(s, &tmp)) {
+        *bl = (bool)tmp;
+    } else {
+        return false;
+    }
+    return true;
+}
+
+const char * const str_auto_bool(auto_bool_t abl)
+{
+    switch (abl) {
+    case BOOL_AUTO:
+        return "auto";
+    case BOOL_TRUE:
+        return "on";
+    case BOOL_FALSE:
+        return "off";
+    }
+    return "unknown";
+}
+
+const char * const str_bool(bool bl)
+{
+    if (bl) {
+        return "on";
+    } else {
+        return "off";
+    }
+}
+
+#define VER_MAJOR          (0)
+#define VER_MINOR          (0)
+#define VER_PATCH          (1)
+
+enum {
+    ERR_PARA            = -1,
+
+};
+
+void usage(void)
+{
+    print("Usage: lrc [OPTIONS]");
+    print(" -h, --help                                      Help");
+    print(" -v, --version                                   Version %d.%d.%d", VER_MAJOR, VER_MINOR, VER_PATCH);
+    print(" -b, --bw,   --bandwidth                 <uint>  Bandwidth in hz, eg: 125000, 250000, 203125");
+    print(" -s, --sf,   --spread-factor             <uint>  Spread factor (5 - 12)");
+    print(" -o, --ldro, --low-datarate-optimize     <bool>  Low datarate optimize (on / off)");
+    print(" -f, --fs,   --fine-synch                <bool>  Fine synch for high speed lora SF5 and SF6, ");
+    print(" -i, --li,   --long-interleaving         <bool>  Available in 2.4GHz lora (sx128x)");
+    print(" -c, --cr,   --coderate                  <uint>  coding rate CR4/5 CR4/6... (1 - 4)");
+    print(" -r, --pr,   --preamble                  <uint>  Raw preamble lenght");
+    print(" -d, --hdr,  --header                    <bool>  Header option (on / off)");
+    print(" -k, --crc,                              <bool>  CRC option (on / off)");
+    print(" -l, --pl,   --payload                   <uint>  payload length");
+}
+
+int main(int argc, char **argv)
+{
+    opt_t app_opt;
+    opt_t *opt;
+    int ret, index, hlen, i;
+    char *ptr;
+    uint32_t tmp;
+    bool bl;
+
+    opt = &app_opt;
+    opt->sf = 7;
+    opt->bw = 125000;
+    opt->cr = 1;
+    opt->ldro = BOOL_AUTO;
+    opt->fs = BOOL_AUTO;
+    opt->li = BOOL_AUTO;
+    opt->pr = 8;
+    opt->hdr = true;
+    opt->crc = true;
+    opt->pl = 12;
+
+    while (1) {
+        ret = getopt_long(argc, argv, ":hvs:b:c:o:f:i:r:d:k:l:", app_long_options, &index);
+        if (ret == -1) {
+            break;
+        }
+        switch (ret) {
+        case 'h':
+            usage();
+            return 0;
+        case 'v':
+            print("v%d.%d.%d", VER_MAJOR, VER_MINOR, VER_PATCH);
+            return 0;
+        case 's':
+            if (!get_u32(optarg, &opt->sf)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'b':
+            if (!get_u32(optarg, &opt->bw)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'c':
+            if (!get_u32(optarg, &opt->cr)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'o':
+            if (!get_bool(optarg, &bl)) {
+                return ERR_PARA;
+            }
+            opt->ldro = bl;
+            break;
+        case 'f':
+            if (!get_bool(optarg, &bl)) {
+                return ERR_PARA;
+            }
+            opt->fs = bl;
+            break;
+        case 'i':
+            if (!get_bool(optarg, &bl)) {
+                return ERR_PARA;
+            }
+            opt->li = bl;
+            break;
+        case 'r':
+            if (!get_u32(optarg, &opt->pr)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'd':
+            if (!get_bool(optarg, &opt->hdr)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'k':
+            if (!get_bool(optarg, &opt->crc)) {
+                return ERR_PARA;
+            }
+            break;
+        case 'l':
+            if (!get_u32(optarg, &opt->pl)) {
+                return ERR_PARA;
+            }
+            break;
+        }
+    }
+
+    double toa = lrc(opt);
+
+    print("toa: %.0fus, %.3fms, %.3fs", toa, toa / 1000, toa / 1000 / 1000);
+
+    print("sf %u, bw %u, cr %u, ldro %s, fs %s, li %s, pr %u, hdr %s, crc %s, pl %u",
+        opt->sf,
+        opt->bw,
+        opt->cr,
+        str_auto_bool(opt->ldro),
+        str_auto_bool(opt->fs),
+        str_auto_bool(opt->li),
+        opt->pr,
+        str_bool(opt->hdr),
+        str_bool(opt->crc),
+        opt->pl
+        );
+
+    return 0;
 }
